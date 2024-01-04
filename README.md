d87c8f8a70c103e6e0c0a0ffca35c3de02851846
Kingshuk Chowdhury <kingshuk.chowdhury@modirum.com>
2024-01-04 19:54:30 +0200

    Terminal Art

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # term_art
+
+Term-Art is an ASCII Art generator written in bash for Linux Terminal. It's quite fun.
+
+## Usage
+```bash
+cd ~
+git clone git@github.com:kingshuk7/term_art.git
+cd term-art
+bash term-art ./standard.txt "Hello"
+```
+The output will be in ASCII Art
+```
+ _    _          _   _          
+| |  | |        | | | |         
+| |__| |   ___  | | | |   ___   
+|  __  |  / _ \ | | | |  / _ \  
+| |  | | |  __/ | | | | | (_) | 
+|_|  |_|  \___| |_| |_|  \___/  
+                                
+                                
+```
+
---
+++ term_art.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Check if there is at least one command-line argument
+if [ "$#" -lt 1 ]; then
+    echo "The script should have at least one argument. Please run the script in a correct way."
+    exit 1
+fi
+
+TextFile="~/term-art/standard.txt"
+
+# Check if a second command-line argument is provided (for the file name)
+if [ "$#" -gt 1 ]; then
+    TextFile="$1" # Get file name from command-line argument
+fi
+
+# Check if the file exists
+if [ ! -e "$TextFile" ]; then
+    echo "File not found: $TextFile"
+    exit 1
+fi
+
+# Read the text to be printed
+text="$2"
+
+# Read the font file and store the lines in an array
+readarray -t fontList < "$TextFile"
+
+# Split the user's input based on the "\\n" delimiter
+IFS=$'\n' read -ra splitText <<< "$text"
+
+# Iterate through each line in the splitText array
+for line in "${splitText[@]}"; do
+    # Check if the line is empty
+    if [ -z "$line" ]; then
+        echo
+        continue
+    fi
+
+    # Loop through 8 times (for each row in the characters)
+    for ((i=0; i<8; i++)); do
+        # Loop through each character in the line
+        for ((j=0; j<${#line}; j++)); do
+            char="${line:$j:1}"
+            # Check if the character is within the ASCII range
+            if [ "$(( $(printf '%d' "'$char")))" -ge 32 ] && [ "$(( $(printf '%d' "'$char")))" -le 126 ]; then
+                # Print the corresponding character from fontList
+                initial="${fontList[i + (( $(printf '%d' "'$char") - 32) * 9) + 1]}"
+                echo -n "$initial"
+            fi
+        done
+        echo # Move to the next row
+    done
+done
